27e8c5913681d29f3926264cddbd5f0bd3263748
IN-Accurate <vishalsankar410@gmail.com>
2023-01-10 00:55:33 +0530

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
-import Login from './pages/Login/Login'
+import "./App.css";
+import HomePage from "./Routes/Home";
+import AuthPage from "./Routes/Auth";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+    errorElement: <div>Something went wrong</div>,
+  },  {
+    path: "/login",
+    element: <AuthPage/>,
+    errorElement: <div>Something went wrong</div>,
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+
+    <RouterProvider router={router} />
     </div>
   );
 }
