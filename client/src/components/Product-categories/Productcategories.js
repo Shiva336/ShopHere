b8f92c8bc513f56c2286f5b1e1e78a8365575962
IN-Accurate <vishalsankar410@gmail.com>
2023-01-22 14:49:50 +0530

    Make product-categories UI better

--- client/src/components/Product-categories/Productcategories.js
+++ client/src/components/Product-categories/Productcategories.js
@@ -5,14 +5,14 @@ import "./Productcategories.css";
 import { GiBurningDot } from "react-icons/gi";
 import Topbar from "../topbar/Topbar";
 import { useNavigate } from "react-router-dom";
-import {AiTwotoneStar} from 'react-icons/ai'
+import { AiTwotoneStar } from "react-icons/ai";
 function Productcategories() {
   const { category } = useParams();
   const navigate = useNavigate();
   const [products, setProduct] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const handleCartClick =() => {}
-  const handleWishlistClick=()=>{}
+  const handleCartClick = () => {};
+  const handleWishlistClick = () => {};
   useEffect(() => {
     (async () => {
       setIsLoading(true);
@@ -31,12 +31,15 @@ function Productcategories() {
   let avgRating = 0;
   let num = 0;
   function displayRating(product) {
-    avgRating=0; num=0
-                    {product.rating.map((rate)=> {
-                      avgRating+= parseFloat(rate);
-                      num = num+1;
-                    })}
-    return (parseInt((avgRating/num)*100))/100
+    avgRating = 0;
+    num = 0;
+    {
+      product.rating.map((rate) => {
+        avgRating += parseFloat(rate);
+        num = num + 1;
+      });
+    }
+    return parseInt((avgRating / num) * 100) / 100;
   }
   return (
     <>
@@ -46,13 +49,20 @@ function Productcategories() {
           {isLoading && <div className="loader"></div>}
           {!isLoading &&
             products.map((product) => (
-              <div className="individual-product-container"  onClick={() => {
-                navigate(`/product/${product._id}`);
-              }}>
+              <div
+                className="individual-product-container"
+                onClick={() => {
+                  navigate(`/product/${product._id}`);
+                }}
+              >
                 <div className="img-container">
-                  <img src={product.img} alt="productimage"></img>
+                  <div className="individual-product-image">
+                    <img src={product.img} className="individual-product-image"alt="productimage"></img>
+                  </div>
                   <div className="individual-highlights">
-                    <h1 className="named-highlights">Highlights of {product.name}</h1>
+                    <h1 className="named-highlights">
+                      Highlights of {product.name}
+                    </h1>
                     {product.highlights.map((highlight) => (
                       <div className="mapped-highlights">
                         <GiBurningDot className="icon-highlights" />
@@ -69,12 +79,23 @@ function Productcategories() {
                     <h3>{product.price}</h3>
                   </div>
                   <div className="individual-rating">
-                  
-                  <span> Rating: {displayRating(product)} <AiTwotoneStar/> </span>
+                    <span>
+                      Rating: {displayRating(product)} <AiTwotoneStar />
+                    </span>
                   </div>
                   <div className="cart-button-container">
-                    <button className="cart-button primary-btn" onClick={handleCartClick}>Add to cart</button>
-                    <button className="wish-button primary-btn" onClick={handleWishlistClick}>Add to wishlist</button>
+                    <button
+                      className="cart-button primary-btn"
+                      onClick={handleCartClick}
+                    >
+                      Add to cart
+                    </button>
+                    <button
+                      className="wish-button primary-btn"
+                      onClick={handleWishlistClick}
+                    >
+                      Add to wishlist
+                    </button>
                   </div>
                 </div>
               </div>
