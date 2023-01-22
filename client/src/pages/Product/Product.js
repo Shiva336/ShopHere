059ed70705d436ebf621f8887b77e317dcc7f948
IN-Accurate <vishalsankar410@gmail.com>
2023-01-22 15:26:15 +0530

    Get reviews and ratings to product page

--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { api } from "../../api";
 import "./Product.css";
 
-function Product(){
+function Product() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -11,9 +11,10 @@ function Product(){
   useEffect(() => {
     (async () => {
       setIsLoading(true);
-      console.log(id)
+      console.log(id);
       const { data } = await api.get(`/product/${id}`);
       setProduct(data);
+      console.log(data);
       setIsLoading(false);
     })();
 
@@ -26,25 +27,40 @@ function Product(){
 
   return (
     <>
-      <div className="products-container" id="home-container">
+      <div className="separate-products-container" id="home-container">
         {isLoading && <div className="loader"></div>}
         {product && (
-          <>
-            <img
-              src={
-                product.img
-              }
-              alt="Product IMG"
-            />
-            <div className="product-details">
-              <h1>{product.name}</h1>
-              <p>{product.highlights}</p>
+          <div className="separate-product-details">
+            <div className="separate-image-container">
+              <img
+                src={product.img}
+                alt="Product IMG"
+                className="separate-product-image"
+              />
             </div>
-          </>
+            <div className="separate-text-container">
+              <h1>{product.name}</h1>
+              <div className="separate-rr-cont">
+                <div className="separate-ratings">
+                  {product.rating.length} ratings
+                </div>
+                <div style={{ padding: "0px 10px" }}>and</div>
+                <div className="separate-reviews">
+                  {product.reviews.length} reviews
+                </div>
+              </div>
+              <div className="separate-hl">
+                <h2 className="hl-header">Specifications</h2>
+                {product.highlights.map((highlight) => (
+                  <div className="separate-highlights">{highlight}</div>
+                ))}
+              </div>
+            </div>
+          </div>
         )}
       </div>
     </>
   );
-};
+}
 
-export default Product
+export default Product;
