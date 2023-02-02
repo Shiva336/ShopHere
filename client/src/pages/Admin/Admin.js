c72a5fe4c2f7a47f12a2c24f8a3e73a587bdd4ed
IN-Accurate <vishalsankar410@gmail.com>
2023-02-02 23:08:19 +0530

    Add admin-check,fix cart onClick

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
 function Admin() {
+  let loginflag = localStorage.getItem("isLoggedIn");
+  let curr_user;
+  if (loginflag == "true") {
+    curr_user = localStorage.getItem("loggedUser");
+  }
   return (
-    <div>Admin</div>
-  )
+    <div>
+      {curr_user === "admin" && <div>admin</div>}
+      {curr_user !== "admin" && <div>Not an admin!</div>}
+    </div>
+  );
 }
 
-export default Admin
+export default Admin;
